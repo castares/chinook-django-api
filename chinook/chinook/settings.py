6673db661e42eb5ec4db78ac56c63792a504cde1
castares <cesar.castares@gmail.com>
2020-04-21 11:34:00 +0200

    updated static_root

--- chinook/chinook/settings.py
+++ chinook/chinook/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles')
 
 # # Storages
 # AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
