4ab53baed7bf6b3c2ff5a394c4e6b0512e5c5aeb
castares <cesar.castares@gmail.com>
2020-04-21 12:26:15 +0200

    removed staticfiles app

--- chinook/chinook/settings.py
+++ chinook/chinook/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
     'rest_framework.authtoken',
