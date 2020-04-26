2f7d4ae54d2da9f084b59c48532eb9edf5396e72
castares <cesar.castares@gmail.com>
2020-04-26 19:16:35 +0200

    v0.1

--- chinook/settings.py
+++ chinook/settings.py
@@ -141,13 +141,4 @@ USE_TZ = True
 
 STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
 
-# # Storages
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_FILE_OVERWRITE = False
-
-# DEFAULT_FILE_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
-# STATICFILES_STORAGE='storages.backends.s3boto3.S3Boto3Storage'
-
 django_heroku.settings(locals())
